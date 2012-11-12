a7ba400c188ed89a218c2db911c69161052e76cb
Michael Jagielo <michaeljagielo@gmail.com>
2012-11-12 01:22:17 -0800

    changed text message

--- ConferenceManager/src/conferenceManager/GitTest.java
+++ ConferenceManager/src/conferenceManager/GitTest.java
@@ -2,6 +2,6 @@ package conferenceManager;
 
 public class GitTest {
 	  public static void main(String[] args) {
-	    System.out.println("Git is fun");
+	    System.out.println("Where is my .dings folder?");
 	  }
 	} 
